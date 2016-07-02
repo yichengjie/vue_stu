03b6e7d09799a0e9817f05401317cc5f45ea5182
yichengjie <626659321@qq.com>
2016-07-02 11:18:32 +0800

    commit

--- demo01/helloworld.js
+++ demo01/helloworld.js
@@ -1,11 +1,34 @@
+'use strict' ;
 /**
  * Created by yicj on 2016/2/19.
  */
 var exampleData ={
-    name:'Vue.js'
+    name:'Vue.js',
+    list:[],
+    showFlag:true
 } ;
-
 var exampleVM = new Vue({
     el:'#example-1',
-    data:exampleData
+    data:exampleData,
+    methods: {
+        greet: function (event) {
+            this.showFlag = !this.showFlag ;
+        }
+    },
+    ready:function(){
+        getAsynData(this.$data) ;
+    }
 }) ;
+
+//模拟异步获取数据
+function getAsynData (FormData){
+    setTimeout(function(){
+        initData(FormData) ;
+    },2000) ;
+}
+
+function initData(FormData){
+    for(var i = 0 ; i < 5 ; i ++){
+        FormData.list.push('yicj' +i) ;
+    }
+}
